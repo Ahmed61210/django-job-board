ddcb1497977264e0ffd95f1aa5250eb13257d73f
Ahmed61210 <Ahmed61210@users.noreply.github.com>
2021-01-06 12:23:02 +0400

    add temlapte and paginations

--- job/views.py
+++ job/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from django.core.paginator import Paginator
 
 # Create your views here.
 
@@ -6,7 +7,12 @@ from .models import Job
 
 def job_list (request):
     job_list = Job.objects.all()
-    context = {"jobs": job_list}
+
+    paginator = Paginator(job_list, 1) # Show 25 contacts per page
+    page_number = request.GET.get('page')
+    page_obj = paginator.get_page(page_number)
+    
+    context = {"jobs": page_obj}
     return render (request, "job/job_list.html", context)
 
 def job_detail (request, id):  
